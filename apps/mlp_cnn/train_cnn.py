14357b98e1edd93fa62c4a89de5f2ac361b1e92d
Abdoulaye-Koroko <abdoulayekoroko@gmail.com>
2023-10-30 17:48:37 +0000

    Udpdate train_cnn.py

--- apps/mlp_cnn/train_cnn.py
+++ apps/mlp_cnn/train_cnn.py
@@ -32,7 +32,7 @@ def train(args):
         
         batch_size = args.batch_size
         
-        tensorboard_name = args.tensorboard_name
+        result_name = args.result_name
         
         model = args.model
         
@@ -109,7 +109,7 @@ def train(args):
         
         since = time.time()
         
-        data_root = "../../data/"
+        data_root = args.data_path
         
         train_samples,val_samples,trainloader,testloader =  read_data_sets(os.path.abspath(data_root + data),batch_size)
         num_iter_per_epoch = len(trainloader)
@@ -292,7 +292,10 @@ if __name__=="__main__":
     parser.add_argument('--data', type=str, default="CIFAR10",
                         help='data name')
     
-    parser.add_argument('--tensorboard_name', type=str, default="kfac",
+    parser.add_argument('--data_path', type=str, default="./data/",
+                        help='path towards the folder containing the data')
+    
+    parser.add_argument('--result_name', type=str, default="kfac",
                         help='tensorbord_name name')
     
     parser.add_argument('--batch_size', type=int, default=64,
@@ -329,5 +332,15 @@ if __name__=="__main__":
     args = parser.parse_args()
     
     results = train(args)
+    
+    output_folder = "apps/mlp_cnn/results/"
+    
+    if not os.path.exists(output_folder):
+        
+        os.makedirs(output_folder)
+        
+    np.save(output_folder + f'{args.result_name}.npy', results) 
+    
+    print(f"The results are saved in {output_folder} under the name {args.result_name}.npy")
 
     
